38afc32df56001fbdf1c6d4393a9e70d6e3e79a4
Shaun Shuster <shaunshu@post.bgu.ac.il>
2023-05-11 10:28:15 +0000

    fixing tests

--- user/t4.c
+++ user/t4.c
@@ -1,15 +1,19 @@
 #include "kernel/types.h"
 #include "kernel/stat.h"
 #include "user/user.h"
-
+int exitStatus = 0;
 void *exectest()
 {
-    int exitStatus = 0;
+
     char *command = "echo";
-    char *argv[] = {"echo", "exec test", 0};
-    exec(command, argv);
-    exitStatus = 1; // clear warning
-    printf("exec failed with status: %d\n", exitStatus);
+    char *argv[] = {"echo", "SUCCESS", 0};
+    int exec_val = exec(command, argv);
+    if (exec_val != -1)
+    {
+        printf("exec failed with status: %d\n", exec_val);
+        exitStatus = 1;
+        return 0;
+    }
     return 0; // warning
 }
 int main(int argc, char **argv)
@@ -17,7 +21,9 @@ int main(int argc, char **argv)
     int kernelStackSize = 4096;
     void *kernelStack = malloc(kernelStackSize);
     kthread_create(exectest, kernelStack, kernelStackSize);
+    if (exitStatus == 0)
+        printf("should go back to terminal\n");
     kthread_exit(0);
-    printf("should go back to terminal\n");
+
     return 0;
 }
